f7260bc435e177225359443b6a7f4acf10208e18
Talita Bernardino Neves Silva <talita.silva@db1.com.br>
2021-10-07 13:20:18 -0300

    curso9aula3

--- Program.cs
+++ Program.cs
@@ -4,31 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Globalization;
 
 namespace ByteBankImportacaoExportacao
+
 {
     partial class Program
     {
         static void Main(string[] args)
         {
-            var enderecoDoArquivo = "contas.txt";
 
-            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-            using (var leitor = new StreamReader(fluxoDeArquivo))
-            {
-                while (!leitor.EndOfStream)
-                {
-                    var linha = leitor.ReadLine();
-                    Console.WriteLine(linha);
-                }
-            }
-
+            CriarArquivoComWriter();
+            Console.WriteLine("Aplicação finalizada. . .");
 
 
             Console.ReadLine();
         }
-
-
-
     }
 }
